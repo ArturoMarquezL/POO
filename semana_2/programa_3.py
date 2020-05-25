164f014744f4bc50f22e107a936cdf9d378d3507
null null <1719110363@utectulancingo.edu.mx>
2020-05-25 00:48:17 +0000

    programa_3.py funciona

---
+++ semana_2/programa_3.py
@@ -0,0 +1,21 @@
+class avion:
+  def __init__(self):
+    print("Atributos")
+    print("color")
+    print("Modelo")
+    print("Aerolineaa")
+    print("Marca")
+    print("motores")
+    print("asientos")
+    print("baños")
+    print("combustible")
+    print("precio")
+  def metodos(self):
+    print("MEtodos")
+    print("Volar")
+    print("Transportar")
+    print("cargar")
+    print("Prender")
+    print("Frenar")
+objeto_avion=avion()
+objeto_avion.metodos()
