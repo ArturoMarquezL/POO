9d8fc0cc54ccd9bf13802606378d473ee2138a77
null null <1719110363@utectulancingo.edu.mx>
2020-05-25 01:41:57 +0000

    programa_8.py funciona

---
+++ semana_2/programa_8.py
@@ -0,0 +1,21 @@
+class calculadora:
+  def __init__(self):
+    print("Atributos")
+    print("color")
+    print("Modelo")
+    print("tipo")
+    print("Marca")
+    print("pantalla")
+    print("teclas")
+    print("tamaño")
+    print("fuente de energia")
+    print("precio")
+  def metodos(self):
+    print("MEtodos")
+    print("Calcular")
+    print("sumar")
+    print("Restar")
+    print("Dividir")
+    print("Prender")
+objeto_calculadora=calculadora()
+objeto_calculadora.metodos()
