065114e8bc2bf4aeda10f2a587d9bb374a34d73b
null null <1719110363@utectulancingo.edu.mx>
2020-05-25 00:34:13 +0000

    programa_2.py funcionando

---
+++ semana_2/programa_2.py
@@ -0,0 +1,22 @@
+class Estudiante:
+  def __init__(self):
+    'atributos'
+    print("Nombre")
+    print("Apellidos")
+    print("Matricula")
+    print("Escuela")
+    print("Grado")
+    print("Grrupo")
+    print("Materias")
+    print("Correra")
+    print("Seguro social")
+    print("Aula")
+  def metodos(self):
+    'Metodos'
+    print("Estudiar")
+    print("Aprender")
+    print("Escuchar")
+    print("Preguntar")
+    print("Deducir")
+Objeto_Estudiante=Estudiante()
+Objeto_Estudiante.metodos()
