fe804e4ffc33474a1f2af31481f8e06669da5791
null null <1719110363@utectulancingo.edu.mx>
2020-05-25 01:34:50 +0000

    programa_7.py funciona

---
+++ semana_2/programa_7.py
@@ -0,0 +1,21 @@
+class coche:
+  def __init__(self):
+    print("Atributos")
+    print("color")
+    print("Modelo")
+    print("estandar")
+    print("Marca")
+    print("automatico")
+    print("asientos")
+    print("tipo de motor")
+    print("combustible")
+    print("precio")
+  def metodos(self):
+    print("MEtodos")
+    print("Manejar")
+    print("Transportar")
+    print("cargar")
+    print("Prender")
+    print("Frenar")
+objeto_coche=coche()
+objeto_coche.metodos()
