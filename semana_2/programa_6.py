4fbfd52a42c78284ed917af70de8e950581a81df
null null <1719110363@utectulancingo.edu.mx>
2020-05-25 01:30:24 +0000

    programa_6.py funciona

---
+++ semana_2/programa_6.py
@@ -0,0 +1,21 @@
+class futbolista:
+  def __init__(self):
+    print("equipo")
+    print("posicion")
+    print("numero")
+    print("altura")
+    print("peso")
+    print("posicion")
+    print("condicion fisica")
+    print("rapides")
+    print("salario")
+    print("Abilidad")
+  def metodos(self):
+    print("MEtodos")
+    print("brincar")
+    print("Saltar")
+    print("correr")
+    print("atacar")
+    print("ganar")
+objeto_futbolista=futbolista()
+objeto_futbolista.metodos()
