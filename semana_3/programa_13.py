535471a50e42dfa881a7895d2e1575f1c07e9c7f
ArturoMarquezL <1719110363@utectulancingo.edu.mx>
2020-06-01 01:55:58 +0000

    programa_15.py funciona

---
+++ semana_3/programa_13.py
@@ -0,0 +1,32 @@
+class avion:
+  
+  color = "blanco"
+  Modelo = "2020"
+  Aerolinea = "Mexico"
+  Marca = "airbus"
+  motores = "2 motores"
+
+  def __init__(self):
+    print("constructor de avion ")
+    
+  def volar (self):
+    print("volar")
+
+  def transportar (self):  
+    print("transportar")
+
+class avionair (avion):
+  def __init__(self):
+    print("constructor avion air")    
+
+objeto = avion() 
+objeto.volar()
+objeto.transportar()
+objeto_air = avionair()
+objeto_air.volar()
+objeto_air.transportar()
+print(objeto_air.color)
+print(objeto_air.Modelo)
+print(objeto_air.Aerolinea)
+print(objeto_air.Marca)
+print(objeto_air.motores)
